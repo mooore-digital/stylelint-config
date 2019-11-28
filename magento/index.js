204de88c38d383e225ef22c79abeda4d703ceaff
GrimLink <sean.grimlink@gmail.com>
2019-11-28 12:10:25 +0100

    IMP: make less version stand on it's own

--- magento/index.js
+++ magento/index.js
@@ -1,48 +1,13 @@
 module.exports = {
-  extends: "@fylgja/stylelint-config/less",
+  extends: "@fylgja/stylelint-config",
   rules: {
-    "at-rule-empty-line-before": [
-      "always",
-      {
-        except: ["blockless-after-same-name-blockless"],
-        ignore: ["after-comment", "first-nested"],
-        ignoreAtRules: ["import", "if", "else"]
-      }
-    ],
-    "rule-empty-line-before": [
-      "always-multi-line",
-      {
-        except: ["first-nested"],
-        ignore: ["after-comment"]
-      }
-    ],
-    "max-nesting-depth": [
-      5,
-      {
-        severity: "warning"
-      }
-    ],
+    "max-nesting-depth": [6, { severity: "warning" }],
+    "media-feature-name-no-unknown": null,
+    "no-extra-semicolons": null,
     "no-descending-specificity": null,
-    "selector-max-class": 5,
+    "selector-max-compound-selectors": null,
     "selector-max-id": 1,
-    "selector-max-type": 4,
-    "selector-max-compound-selectors": [
-      6,
-      {
-        severity: "warning"
-      }
-    ],
-    "selector-max-specificity": [
-      "1,5,2",
-      {
-        severity: "warning"
-      }
-    ],
-    "selector-no-qualifying-type": [
-      true,
-      {
-        ignore: ["attribute", "class"]
-      }
-    ]
+    "selector-max-class": 5,
+    "selector-max-type": 4
   }
 };
