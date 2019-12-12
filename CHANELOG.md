853c66fbfc6b867b7367ee579b7e6bc1c59838f2
Sean van Zuidam (Grimlink) <sean.grimlink@gmail.com>
2019-12-12 09:24:24 +0100

    IMP: change scss version to the Fylgja version with upsets

--- CHANELOG.md
+++ CHANELOG.md
@@ -1,16 +1,19 @@
 # Changelog
 
-## 1.2.0 - 28-11-19
+## 1.3.0 - (2019-12-12)
+* IMP: change scss version to the Fylgja version with upsets
+
+## 1.2.0 - (2019-11-28)
 * UP: versions for dependencies
 * IMP: Renamed package
 
-## 1.1.1 - 19-05-19
+## 1.1.1 - (2019-05-19)
 * UP: versions for dependencies
 
-## 1.1.0 - 14-04-19
+## 1.1.0 - (2019-04-14)
 * IMP: versions for dependencies
   * See latest changes to [getFylgja/stylelint-config](https://github.com/getfylgja/stylelint-config/releases/)
   * Make dependency patch version a wildcard
 
-## 1.0.0 - 25-02-19
+## 1.0.0 - (2019-02-25)
 * Initial release
--- scss/index.js
+++ scss/index.js
@@ -1,11 +1,39 @@
 module.exports = {
-  extends: "@fylgja/stylelint-config/scss",
+  extends: ["@fylgja/stylelint-config/scss"],
   rules: {
-    "max-nesting-depth": [
-      5,
+    "scss/at-else-closing-brace-newline-after": ["always-last-in-chain", { severity: "warning" }],
+    "scss/at-else-closing-brace-space-after": ["always-intermediate", { severity: "warning" }],
+    "scss/at-else-empty-line-before": ["never", { severity: "warning" }],
+    "scss/at-else-if-parentheses-space-before": ["always", { severity: "warning" }],
+    "scss/at-extend-no-missing-placeholder": null,
+    "scss/at-function-parentheses-space-before": ["never", { severity: "warning" }],
+    "scss/at-if-closing-brace-newline-after": ["always-last-in-chain", { severity: "warning" }],
+    "scss/at-if-closing-brace-space-after": ["always-intermediate", { severity: "warning" }],
+    "scss/at-if-no-null": null,
+    "scss/at-import-no-partial-leading-underscore": true,
+    "scss/at-mixin-argumentless-call-parentheses": null,
+    "scss/at-mixin-parentheses-space-before": null,
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": [true, { severity: "warning" }],
+    "scss/dollar-variable-colon-newline-after": null,
+    "scss/dollar-variable-colon-space-after": null,
+    "scss/dollar-variable-colon-space-before": null,
+    "scss/dollar-variable-no-missing-interpolation": null,
+    "scss/double-slash-comment-whitespace-inside": "always",
+    "scss/function-color-relative": [
+      true,
       {
-        severity: "warning"
+        severity: "warning",
+        message: "Use the new scss color functions instead!"
       }
-    ]
+    ],
+    "scss/media-feature-value-dollar-variable": [
+      "always",
+      { severity: "warning" }
+    ],
+    "scss/operator-no-newline-after": null,
+    "scss/operator-no-newline-before": [true, { severity: "warning" }],
+    "scss/operator-no-unspaced": null,
+    "scss/selector-no-redundant-nesting-selector": [true, { severity: "warning" }]
   }
 };
