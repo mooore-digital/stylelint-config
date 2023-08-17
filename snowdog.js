41da8192ea1cd1e4f121ccbf4612ced6a08ed022
grimlink <sean.grimlink@gmail.com>
2023-08-17 17:19:56 +0200

    ADD: support for less and hyva themes in the snowdog config rules

--- snowdog.js
+++ snowdog.js
@@ -1,8 +1,35 @@
 module.exports = {
   extends: [
+    "stylelint-config-standard",
+    "stylelint-config-html",
+    "@fylgja/stylelint-config/rules/default",
     "./rules/default",
-    "./rules/snowdog",
-    "./rules/scss",
-    "stylelint-config-html/php",
+  ],
+  overrides: [
+    {
+      files: ["*.scss", "**/*.scss"],
+      extends: [
+        "stylelint-config-standard-scss",
+        "@fylgja/stylelint-config/rules/default",
+        "@fylgja/stylelint-config/rules/scss",
+        "./rules/snowdog",
+        "./rules/scss",
+      ],
+    },
+    {
+      files: ["*.less", "**/*.less"],
+      extends: ["./rules/luma"],
+    },
+    {
+      files: [
+        "tailwind*.{css,scss}",
+        "tailwind/*.{css,scss}",
+        "tailwind/**/*.{css,scss}",
+        "**/tailwind*.{css,scss}",
+        "**/tailwind/*.{css,scss}",
+        "**/tailwind/**/*.{css,scss}",
+      ],
+      extends: ["@fylgja/stylelint-config/rules/tailwind"],
+    },
   ],
 };
